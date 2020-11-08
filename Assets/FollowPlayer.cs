3306a81c3d170153c9a29904cc59271332df5125
Pritheev-Roshan <s10193030@connect.np.edu.sg>
2020-11-09 01:28:31 +0800

    Camera Switch Draft 1

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform player;
+    private Vector3 CameraPos;
+
+    private void Start()
     {
-        
+        // Sets camera positions
+        CameraPos = player.position;
+        transform.position = CameraPos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetAxisRaw("Mouse ScrollWheel") > 0)
+        {
+            // Scroll Wheel goes up
+            //transform.position = player.position;
+            CameraPos = player.position;
+            Debug.Log("Mouse Up");
+
+        }
+        else if (Input.GetAxisRaw("Mouse ScrollWheel") < 0)
+        {
+            // Scroll Wheel goes down
+            Vector3 offset = new Vector3(0, 1, -5);
+            //transform.position = player.position + offset;
+            CameraPos = player.position + offset;
+            Debug.Log("Mouse Down");
+        }
     }
 }
