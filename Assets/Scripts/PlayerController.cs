ecefb49e150e0dce3c78b798c57530180246aad4
Pritheev-Roshan <s10193030@connect.np.edu.sg>
2020-11-11 11:57:55 +0800

    Created Working PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,16 +3,26 @@
 public class PlayerController : MonoBehaviour
 {
 
+	public CharacterController controller;
+	public float speed = 12f;
+	/*
 	// This is a reference to the Rigidbody component called "rb"
 	public Rigidbody rb;
 
 	public float forwardForce = 2000f;  // Variable that determines the forward force
 	public float sidewaysForce = 500f;  // Variable that determines the sideways force
-
+	*/
 	// We marked this as "Fixed"Update because we
 	// are using it to mess with physics.
 	void FixedUpdate()
 	{
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		Vector3 move = transform.right * x + transform.forward * z;
+
+		controller.Move(move * speed * Time.deltaTime);
+		/*
 		// Add a forward force
 		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
@@ -39,6 +49,7 @@ public class PlayerController : MonoBehaviour
 			// Add a force to the left
 			rb.AddForce(0, 0, - forwardForce * Time.deltaTime,ForceMode.VelocityChange);
 		}
+		*/
 
 	}
 }
