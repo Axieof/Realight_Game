1f006fa7cf7e04c54aac33ec6b6933e61f93de69
Axieof <pritheev24@gmail.com>
2020-11-13 11:08:54 +0800

    Added Third Person Movement Functionality

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public Vector3 offset;
+    public Transform player;
+    public GameObject FirstPersonCamera;
+    public GameObject ThirdPersonCamera;
 
     public float speed = 12f;
     public float gravity = -9.81f;
+    public float turnSmoothTime = 0.1f;
+    public float turnSmoothVelocity;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
@@ -16,8 +24,33 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
+    private void Start()
+    {
+        // Sets camera positions
+        //CameraPos = player.position;
+        offset = new Vector3(0, 1, -5);
+        transform.position = player.position;
+
+        FirstPersonCamera.SetActive(true);
+        ThirdPersonCamera.SetActive(false);
+    }
+
     // Update is called once per frame
     void Update()
+    {
+        if (FirstPersonCamera.activeSelf)
+        {
+            Debug.Log("First Person!");
+            FirstPerson();
+        }
+        else if (ThirdPersonCamera.activeSelf)
+        {
+            Debug.Log("Third Person!");
+            ThirdPerson();
+        }
+    }
+
+    void FirstPerson()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
@@ -42,4 +75,21 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(velocity * Time.deltaTime);
     }
+
+    void ThirdPerson()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+
 }
