232a2a3e5582bb7cfd618e77d28ed21ce742456e
Pritheev-Roshan <s10193030@connect.np.edu.sg>
2020-11-18 16:54:59 +0800

    Added ConnectionTest

---
+++ Assets/Scripts/ConnectionTest.cs
@@ -0,0 +1,27 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionTest : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("Connecting to server...");
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Disconencted from server for reason " + cause.ToString());
+    }
+
+}
