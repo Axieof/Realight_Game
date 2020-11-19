61b24d257c28bc8a037a3213005dfc54dd1a0b9d
Lee Yan Han <s10194905@connect.np.edu.sg>
2020-11-19 10:03:20 +0800

    Made UI for game

---
+++ Assets/Scripts/DisableAnimationScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableAnimationScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindWithTag("LevelLoader").GetComponent<LoadScene>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public Animator animator;
+    public float transitionTime  = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && !(Input.GetMouseButtonDown(0)
+            || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2)))
+        {
+            LoadTransition();
+        }
+    }
+    public void LoadTransition()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        animator.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
