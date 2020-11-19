e442f4902e81ffc485329c8786419edc6fd07fa9
Lee Yan Han <s10194905@connect.np.edu.sg>
2020-11-19 14:59:36 +0800

    Added PlayerNameInput Script

---
+++ Assets/Scripts/Menu/PlayerNameInput.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class PlayerNameInput : MonoBehaviour
+{
+    [SerializeField] private Text nameInputField = null;
+    [SerializeField] private Button CreateButton = null;
+    [SerializeField] private Button JoinButton = null;
+
+    private const string PlayerPrefsNameKey = "PlayerName";
+
+    private void Start() => SetUpInputField();
+
+    private void SetUpInputField()
+    {
+        if (!PlayerPrefs.HasKey(PlayerPrefsNameKey)) { return; }
+
+        string defaultName = PlayerPrefs.GetString(PlayerPrefsNameKey);
+
+        nameInputField.text = defaultName;
+
+        SetPlayerName(defaultName);
+    }
+
+    public void SetPlayerName(string name)
+    {
+        CreateButton.interactable = !string.IsNullOrEmpty(name);
+        JoinButton.interactable = !string.IsNullOrEmpty(name);
+    }
+
+    public void SavePlayerName()
+    {
+        string playerName = nameInputField.text;
+
+        PhotonNetwork.NickName = playerName;
+
+        PlayerPrefs.SetString(PlayerPrefsNameKey, playerName);
+    }
+}
