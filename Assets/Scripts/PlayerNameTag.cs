d2996e86d8e09077f28dfb6d97cd0cdbf12bd055
Pritheev-Roshan <s10193030@connect.np.edu.sg>
2020-11-20 02:55:03 +0800

    Added Player NameTag

---
+++ Assets/Scripts/PlayerNameTag.cs
@@ -0,0 +1,26 @@
+﻿using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerNameTag : MonoBehaviourPun
+{
+    [SerializeField] private TextMeshProUGUI nameText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (photonView.IsMine)
+        {
+            return;
+        }
+        SetName();
+    }
+
+    private void SetName()
+    {
+        nameText.text = photonView.Owner.NickName;
+    }
+
+}
