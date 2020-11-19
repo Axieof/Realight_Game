1e5d330060508475b58b2b9900f5778e247a355b
Pritheev-Roshan <s10193030@connect.np.edu.sg>
2020-11-19 11:26:54 +0800

    Started Server Side Connection

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Successfully Conencted to Master!");
+    }
+
+    
+}
