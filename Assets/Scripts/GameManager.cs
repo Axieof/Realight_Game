14a544175d79da0cdd0d615875894e7420808416
Axieof <s10193030@connect.np.edu.sg>
2021-01-06 09:55:22 +0800

    Updated GameManager Script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
     public static GameManager instance;
 
     public static Dictionary<int, PlayerManager> players = new Dictionary<int, PlayerManager>();
