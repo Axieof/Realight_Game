d9d52324758eff3eb220e15f79a08fbe42c2e0f7
Axieof <pritheev24@gmail.com>
2021-01-29 09:34:58 +0800

    Revert "Merge branch 'Networking' of https://github.com/Axieof/Realight_Game into Networking"
    This reverts commit cfb921c31098f9d06ce3daab4aa94789d6875d70, reversing
    changes made to 32eb1833985dae5e46b581a3908b6008005fa1a2.

--- Assets/Scripts/Menu/CreateRoom.cs
+++ Assets/Scripts/Menu/CreateRoom.cs
@@ -17,7 +17,6 @@ public class CreateRoom : MonoBehaviourPunCallbacks
     public string interest;
     public bool publicIsPressed = false;
     public bool privateIsPressed = false;
-    public System.Random rdm = new System.Random();
 
     public void Start()
     {
@@ -50,15 +49,6 @@ public class CreateRoom : MonoBehaviourPunCallbacks
         publicIsPressed = false;
     }
 
-    public int GenerateRandomCode()
-    {
-        int minimum = 1000;
-        int maximum = 9999;
-
-        int code =  rdm.Next(minimum, maximum);
-        return code;
-    }
-
     public void Create()
     {
         RoomOptions roomOptions = new RoomOptions()
@@ -89,8 +79,6 @@ public class CreateRoom : MonoBehaviourPunCallbacks
             PhotonNetwork.SetMasterClient(masterClientPlayer);
             //PhotonNetwork.LoadLevel(1);
             Debug.LogFormat("Region {0}", PhotonNetwork.NetworkingClient.CloudRegion);
-            int code = GenerateRandomCode();
-            Debug.Log("Code is " + code);
         }
         else
         {
