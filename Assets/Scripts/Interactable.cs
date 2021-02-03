5f6e4dad06ab2dc0688cd59c2b483b6c1314394b
Si Min Nicole Sim <s10196790@connect.np.edu.sg>
2021-02-03 23:06:47 +0800

    made hills

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    //public float radius = 3f;
+
+    //void OnDrawGizmosSelected() {
+        //Gizmos.colour = Color.Yellow;
+        //Gizmos.DrawWireSphere(transform.position, radis);
+    //} 
+}
